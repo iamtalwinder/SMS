cfec59b3bce99b5c18dbc73370575eb09667d6d2
Talwinder Singh <singhtalwinder790@gmail.com>
2020-04-16 22:52:21 -0800

    checking continuous integration

--- routes/logout.js
+++ routes/logout.js
@@ -6,7 +6,7 @@ router.delete('/logout', checkToken, all, (req, res)=>{
     const token = req.header('auth-token');
     con.query(`DELETE FROM token WHERE auth_token="${token}"`, (err, result)=>{
         if (err) return res.status(400).send(err);
-        res.send('Logged out!');
+        res.send('Logged out!!!');
     }); 
 });
 
