3b5bdddd54c58aab52d77b5387ff7ed1a98aeac7
Talwinder Singh <singhtalwinder790@gmail.com>
2020-04-17 19:21:16 -0800

    adding frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT;
 //Middleware
 app.use(express.json());
@@ -13,6 +14,10 @@ app.use('/api/user/student', require('./routes/studentInfo'));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+
+    app.get('*', (req, res)=>{
+        res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.listen(PORT, () => console.log(`Running on PORT: ${process.env.PORT}`));
